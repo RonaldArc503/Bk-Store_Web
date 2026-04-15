de9ae519f61ecc21a834520f2d6e7f2950d74711
andrea.guardado <144756278+andieherre@users.noreply.github.com>
2026-04-14 19:39:25 -0600

    añadir componetes

---
+++ src/components/input.tsx
@@ -0,0 +1,20 @@
+// Ejemplo de cómo debe ser tu componente Input para que no falle
+import * as React from "react"
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={className}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
